b4bf2e4556637a356a13c3a5b4724f6b47c71029
Kevin Chan <kevin.chan@Perficient.com>
2019-02-25 14:10:00 -0500

    added remove template"

---
+++ js/news_quickview_template.js
@@ -0,0 +1,16 @@
+  <div>This is a custom quick view template that can be modified.</div>
+  <div>
+    <span><strong>News Source:</strong></span><span class="CoveoFieldValue" data-field="@news_source" data-html-value="true">
+    </span>
+  </div>
+  <div>
+    <span><strong>Author:</strong></span><span class="CoveoFieldValue" data-field="@news_author" data-html-value="true">
+    </span>
+  </div>
+  <div>
+    <span><strong>News Section:</strong></span><span class="CoveoFieldValue" data-field="@news_section" data-html-value="true">
+    </span>
+  </div>
+  <div>
+    <span><strong>Link to article (no href bound):</strong></span><a href=""><span class="CoveoFieldValue" data-field="@uri" data-html-value="true"><a></span>
+  </div>
