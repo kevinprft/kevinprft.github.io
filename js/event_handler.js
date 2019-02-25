bae7715cdfacffd726295c66c5d84e633a134059
Kevin Chan <kevin.chan@Perficient.com>
2019-02-25 14:59:13 -0500

    js update

--- js/event_handler.js
+++ js/event_handler.js
@@ -2,18 +2,29 @@
   var searchBox = document.getElementById('search');
   var customSearchBox = document.getElementById('customSearchBox');
   var customSearchButton =document.getElementById('customSearchButton');
-
-
-  function switch_pipeline(pipeline_name){
-    var searchRoot = document.getElementById('search');
-    searchRoot.setAttribute("pipeline", pipeline_name);
-  }
+  var piplineSelector1 = document.getElementById('pipeline-selector1');
+  var piplineSelector2 = document.getElementById('pipeline-selector2');
 
   function logEvent(message){
     var dt = new Date();
     var utcDate = dt.toUTCString();
     console.log(("Debug [" + utcDate + "] ") + message);
   }
+
+  function switch_pipeline(pipeline_name){
+    var searchRoot = document.getElementById('search');
+    searchRoot.setAttribute("pipeline", pipeline_name);
+    logEvent('Pipeline switched: ' + pipeline_name);
+    Coveo.init(searchRoot);
+  }
+
+  piplineSelector1.addEventListener('click', function(){
+    switch_pipeline('kevin_pipeline');
+  });
+
+  piplineSelector2.addEventListener('click', function(){
+    switch_pipeline('kevin_pipeline_2');
+  });
   
   customSearchButton.addEventListener('click', function(event){
     logEvent("Executing: " + customSearchBox.value);
