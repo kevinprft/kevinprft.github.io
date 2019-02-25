62cf0b6f57a8054d46621e48d640b0d7210d4409
Kevin Chan <kevin.chan@Perficient.com>
2019-02-25 15:07:58 -0500

    up

--- js/event_handler.js
+++ js/event_handler.js
@@ -23,18 +23,22 @@
     switch_pipeline('kevin_pipeline_2');
   });
   
-  Coveo.$$(search).on('doneBuildingQuery', function(event, args){
-    logEvent("Query Building Completed");
-    console.log(args);
-  })
-  
-  Coveo.$$(search).on('duringQuery', function(event, args){
-    logEvent("duringQuery");
-    console.log(args);
-  })
+  Coveo.$$(search).on('afterComponentsInitialization', function(event, args){
+    logEvent("afterComponentsInitialization");
 
-  Coveo.$$(search).on('querySuccess', function(event, args){
-    logEvent("querySuccess");
-    console.log(args);
+    Coveo.$$(search).on('doneBuildingQuery', function(event, args){
+      logEvent("Query Building Completed");
+      console.log(args);
+    });
+    
+    Coveo.$$(search).on('duringQuery', function(event, args){
+      logEvent("duringQuery");
+      console.log(args);
+    });
+  
+    Coveo.$$(search).on('querySuccess', function(event, args){
+      logEvent("querySuccess");
+      console.log(args);
+    });
   })
   
